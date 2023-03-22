1b9d76c117e31305ac06a12f67dbea77b8f76f1d
Kushansewmina2580 <112306462+Kushansewmina2580@users.noreply.github.com>
2023-03-22 18:49:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,8 +12,8 @@ LANG: process.env.LANG === undefined ? 'EN' : process.env.LANG,
 ALIVE_LOGO: process.env.ALIVE_LOGO === undefined ? 'https://i.imgur.com/v8iuP8v.jpg' : process.env.ALIVE_LOGO,
 FOOTER: process.env.FOOTER === undefined ? 'ᴘᴜʙʟɪᴄ ꜱᴛᴀʙʟᴇ' : process.env.FOOTER,
 CAPTION: process.env.CAPTION === undefined ? '*ɢᴇɴᴀʀᴀᴛᴇᴅ ʙʏ KUSHAN*' : process.env.CAPTION,
-ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'false' : process.env.ONLY_GROUP,
+ONLY_GROUP: process.env.ONLY_GROUP === undefined ? 'true' : process.env.ONLY_GROUP,
 INBOX_BL_MSG: process.env.INBOX_BL_MSG === undefined ? '*Inbox not allowed' : process.env.INBOX_BL_MSG,
 HOST: process.env.HOST === undefined ? 'MOGENIUS' : process.env.HOST,
-SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=xyhSQajY#MdWBrq-Wwhtg3xqG-yIm9ZskpHobHLpSn4efQJC9A6g' : process.env.SESSION_ID
+SESSION_ID: process.env.SESSION_ID === undefined ? 'ASTRO=8ypzhBbB#hJujtvrn1C_obmobyWZHxZu7ktL92djtmjMZY9TmaF0' : process.env.SESSION_ID
 };
